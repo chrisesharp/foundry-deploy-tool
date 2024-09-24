b75eb8a380d8d1de84b6f6927cf60a088996df5a
Chris Sharp <chrisesharp@gmail.com>
2024-09-24 13:50:00 +0100

    refactor env

--- terraform/install.sh
+++ terraform/install.sh
@@ -6,7 +6,7 @@ if [ -z ${FOUNDRY_USERNAME} ]; then echo "FOUNDRY_USERNAME is unset" ; exit ; fi
 if [ -z ${FOUNDRY_PASSWORD} ]; then echo "FOUNDRY_PASSWORD is unset" ; exit ; fi
 
 export TF_VAR_do_token=${DO_PAT}
-export TF_VAR_pvt_key=$HOME/.ssh/mac_token
+export TF_VAR_pvt_key=${PVT_KEY}
 export TF_VAR_foundry_user=${FOUNDRY_USERNAME}
 export TF_VAR_foundry_password=${FOUNDRY_PASSWORD}
 
@@ -14,7 +14,7 @@ worldbundler
 
 if [ $? -eq 0 ]
 then
-    # terraform apply -var "do_token=${DO_PAT}" -var "pvt_key=$HOME/.ssh/mac_token" -var "foundry_user=${FOUNDRY_USERNAME}" -var "foundry_password=${FOUNDRY_PASSWORD}" -auto-approve
+    # terraform apply -var "do_token=${DO_PAT}" -var "pvt_key=${PVT_KEY}" -var "foundry_user=${FOUNDRY_USERNAME}" -var "foundry_password=${FOUNDRY_PASSWORD}" -auto-approve
     terraform apply -auto-approve
 else
     exit
